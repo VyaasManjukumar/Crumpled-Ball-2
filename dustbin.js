5fe305fc5b74f9cf5ad23e954c4891144300a9ae
VyaasManjukumar <73779392+VyaasManjukumar@users.noreply.github.com>
2021-01-02 18:11:55 +0000

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,42 @@
+class trashcan{
+
+    constructor(x,y,width,height){
+
+super(x,y,width,height);
+       this.image=loadImage("image/dustbin.png");
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        
+        this.LeftSide = Bodies.rectangle(this.x,this.y,this.width,this.height,options);
+        World.add(world,this.LeftSide);
+
+        this.RightSide = Bodies.rectangle(this.x+150,this.y,this.width,this.height,options);
+        World.add(world,this.RightSide);
+
+        this.BottomSide = Bodies.rectangle(this.x+75,this.y+40,this.height+50,this.width,options);
+        World.add(world,this.BottomSide);
+
+
+
+    }
+
+
+
+    display(){
+        super.display
+
+        rectMode(CENTER);
+        fill("green");
+        rect(this.BottomSide.position.x,this.BottomSide.position.y,this.height+50,this.width);
+        
+        rect(this.LeftSide.position.x,this.LeftSide.position.y,this.width,this.height);
+
+        rect(this.RightSide.position.x,this.RightSide.position.y,this.width,this.height);
+
+       
+       
+    }
+};
---
+++ paper.js
@@ -0,0 +1,10 @@
+class Paper {
+    constructor(x, y,r) {
+      super(x,y,r);
+      this.image=loadImage("sprites/paper.png");
+    };
+    display(){
+      
+      super.display();
+    }
+  };
